ab084a241da65603908e806c2c38a72738aed105
Nathanael Mario Duarte Delfino <nathanael.delfino@live.com>
2024-02-09 18:38:05 -0300

    Atualizar passos de teste para usar a biblioteca @cucumber/cucumber

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,22 +1,25 @@
 const assert = require("assert");
-const { Given, When, Then } = require("cucumber");
+const {Given,When, Then } = require("@cucumber/cucumber");
 
 const  isItFriday = today => {
     return today === 'Friday' ? 'TGIF' : 'Nope';
 }
 
-Given('today is Sunday', function () {
-    this.today = 'Sunday';
+
+When("I ask whether it's Friday yet", () => {
+  this.actualAnswer = isItFriday(this.today);
 });
 
-When('I ask whether it\'s Friday yet', function () {
-    this.actualAnswer = isItFriday(this.today);
-});
-
-Then('I should be told {string}', function (string) {
-    assert.equal(this.actualAnswer, string);
-});
 
 Given('today is Friday', () => {
     this.today = 'Friday';
+})
+
+
+Given('today is Sunday', () => {
+  this.today = 'Sunday';
+})
+
+Then('I should be told {string}', (s) => {
+    assert.strictEqual(this.actualAnswer, s);
 })
